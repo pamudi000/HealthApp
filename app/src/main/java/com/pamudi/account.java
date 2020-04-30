352a3cd1b6b58adf932fdc2826d2fc75e093cea6
User <pamudiramkumar@gmail.com>
2020-04-30 22:17:50 +0530

    account java change

--- app/src/main/java/com/pamudi/account.java
+++ app/src/main/java/com/pamudi/account.java
@@ -2,13 +2,16 @@ package com.pamudi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.chamathka.medicinereminder.R;
 
 public class account extends AppCompatActivity {
     DatabaseHelper db;
